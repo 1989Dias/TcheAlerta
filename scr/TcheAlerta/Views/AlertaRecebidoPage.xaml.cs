d10226e64f0c418a2c721da335f34e62e3b16fd6
Patrícia Conceição <1989dias@gmail.com>
2021-05-17 00:27:13 -0300

    first commit 🎉

---
+++ scr/TcheAlerta/Views/AlertaRecebidoPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TcheAlerta.ViewModels;
+using TcheAlerta.ViewModels.Alertas;
+using Xamarin.Forms;
+using Xamarin.Forms.OpenWhatsApp;
+using Xamarin.Forms.Xaml;
+
+namespace TcheAlerta.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AlertaRecebidoPage : ContentPage
+    {
+        private readonly IPaginaServico _paginaServico;
+
+        public AlertaRecebidoPage() {
+            _paginaServico = new PaginaServico();
+            InitializeComponent();
+        }
+
+        public AlertaRecebidoPage(string IdAlerta) {           
+            InitializeComponent();
+            BindingContext = new AlertaViewModel(IdAlerta);
+        }
+       
+    }
+}
---
+++ scr/TcheAlerta/Views/NovoAlertaPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using TcheAlerta.ViewModels.Alertas;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TcheAlerta.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NovoAlertaPage : ContentPage {
+        AlertaViewModel viewModel;
+
+        public NovoAlertaPage() {
+            viewModel = new AlertaViewModel();
+            BindingContext = new AlertaViewModel();
+           
+            InitializeComponent();            
+        }
+    }
+}
